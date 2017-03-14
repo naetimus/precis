8cc8e44afcd55c40d44d67176b411741f8e88a36
Shubham Jain <shubham7jain@gmail.com>
2017-03-14 17:14:08 -0500

    Added integration

--- webapp/index.php
+++ webapp/index.php
@@ -62,7 +62,7 @@
             data: { 
                'foo': 'bar', 
                'ca$libri': 'no$libri' // <-- the $ sign in the parameter name seems unusual, I would avoid it
-            }
+            },
             success: function(data) {
                 //show content
                 alert('Success!');
